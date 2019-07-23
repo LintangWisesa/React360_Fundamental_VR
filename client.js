73b0c4f7a3ca571e394a5372ba5f84677cf28f51
lintangwisesa <lintangwisesa@ymail.com>
2019-07-23 14:50:44 +0700

    react360

--- client.js
+++ client.js
@@ -1,9 +1,7 @@
 // This file contains the boilerplate to execute your React app.
 // If you want to modify your application's content, start in "index.js"
 
-import {ReactInstance, Location} from 'react-360-web';
-
-
+import {ReactInstance} from 'react-360-web';
 
 function init(bundle, parent, options = {}) {
   const r360 = new ReactInstance(bundle, parent, {
